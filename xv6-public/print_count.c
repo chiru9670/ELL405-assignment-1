bccbe481f65dec90f3f286e7565b1eb7a2b8ae85
Anirudh Panigrahi <chiru9670@gmail.com>
2021-03-07 18:15:50 +0530

    Clean up some executable names Now have seperate executables to test toggle, print_count and ps

--- xv6-public/print_count.c
+++ xv6-public/print_count.c
@@ -6,15 +6,6 @@ int main(int argc , char *argv [])
 // If you follow the naming convention , the system call
 // name will be sys_toggle and you
 // call it by calling the function toggle ();
-    toggle ();
-    //add(1,2);
-    //add(1,2);
-    //add(1,2);
-    ps();
-    //print_count();
-    //toggle();
-    //toggle();
-    //add(1,1);
-    //toggle();
+    print_count();
     exit();
 }
