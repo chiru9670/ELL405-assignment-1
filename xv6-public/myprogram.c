57df845b99b74ad453fefaf88428e6f5f94d71ab
Harsh <hwardhan337@gmail.com>
2021-03-07 15:58:06 +0530

    toggle and print count added

---
+++ xv6-public/myprogram.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+int main(int argc , char *argv [])
+{
+// If you follow the naming convention , the system call
+// name will be sys_toggle and you
+// call it by calling the function toggle ();
+    toggle ();
+    add(1,2);
+    add(1,2);
+    add(1,2);
+    print_count();
+    toggle();
+    exit();
+}
